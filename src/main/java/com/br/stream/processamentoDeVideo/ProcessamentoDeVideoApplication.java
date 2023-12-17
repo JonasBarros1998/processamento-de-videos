26c4b8ea39b21682ee19275ef41d7b2ea89e47da
Jonas Barros <Jonas_barros@outlook.com>
2023-12-17 16:48:40 -0300

    first commit

---
+++ src/main/java/com/br/stream/processamentoDeVideo/ProcessamentoDeVideoApplication.java
@@ -0,0 +1,13 @@
+package com.br.stream.processamentoDeVideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessamentoDeVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessamentoDeVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/stream/processamentoDeVideo/ProcessamentoDeVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.stream.processamentoDeVideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessamentoDeVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
